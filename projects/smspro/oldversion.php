0e3a8d694cc9fbf3bc89c7ec31ef4ef5eed3dd63
saycoder <52132780+saycoder@users.noreply.github.com>
2024-10-13 22:49:51 +0330

    Add files via upload

---
+++ projects/smspro/oldversion.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+ <html dir="rtl" lang="fa">
+<head>
+<title>نسخه های قدیم پیام ها</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="https://myket.ir/app-icon/com.saycoder.smsmanager_38f4d0b4-7c38-4b72-a773-ba73c532b287.png" style="width:100%"  alt="پیام ها">
+          <br>
+                    <br>
+                    <br>
+
+          <br>
+
+          <div class="w3-display-bottomright w3-container w3-text-black">
+            <h2>اپلیکیشن پیام ویژه</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>مشخصات پشتیبانی داخل برنامه</p>
+          <hr>
+
+          <p>پیشرفت کار</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>نسخه های قدیمی</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>انواع گوشی ها سیاست های امنیتی متنوع!</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>فروردین 1402 <span class="w3-tag w3-teal w3-round">نسخه فعلی</span></h6>
+          <p>
+         تا اسفند 1401 نزدیک به 30 نسخه پیام ویژه منتشر شده بود و دست زدن به نسخه استیبلی که حاصل کار و بازخورد و نظرات نزدیک به چهار هزار نفر در مارکت های مختلف بود کار سختی به نظر می رسید تا اینکه اندروید نسخه 10 و بالاتر باعث ناسازگاری شد و دست به تغییراتی اجباری در برنامه زدیم اما همین تغییرات اجباری باعث بروز اختلال در بعضی گوشی ها مثل شیائومی شد.
+          از آنجایی تست دقیق برنامه در انواع گوشی ها برای برنامه نویس ممکن نیست بعضی خطاها هنوز پا برجا هستند برای مثال برای تست در گوشی شائومی برنامه نویس باید حدود 15میلیون تومان صرف خرید این گوشی کند در حالی که کل فروش این برنامه در یک سال کمتر از 5 میلیون می باشد(بیشتر قابلیت ها رایگان است و احتمالا کاربران تمایلی به خرید نسخه کامل ندارند) لذا ضمن پوزش از کاربرانی که برنامه در گوشی آن ها کار نمی کند نسخه های قدیمی برنامه را در این صفحه قرار می دهیم تا در صورت تمایل از آن ها استفاده کنند.<br> توجه نمایید که با نصب نسخه قدیمی نباید برنامه را بروز رسانی کنید همچنین احتمالا نیاز باشد نسخه جدیدتر را حذف نمایید.
+          </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>نسخه استیبل   ورژن 6.0.0 </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>1400/08/17 </h6>
+          <p>
+          نسخه 6.0.0 برنامه در تاریخ 17آبان1400 منتشر شده و نسخه استیبلی است اگر نسخه فعلی روی گوشی شما کار نمی کند می توانید از این نسخه استفاده کنید.
+          نسخه کامل: 
+برای بازیابی خرید خود حتما نسخه ای را دانلود کنید که قبلا از آن فروشگاه خریداری کرده اید مثلا اگر قبلا از بازار خرید کردید اینجا هم نسخه بازار را دانلود کنید یا اگر از مایکت خریدید نسخه مایکت را دانلود کنید
+
+                           <br>
+     <br>
+          <a href="https://saycoder.com/downloads/پیام-ویژه-ورژن کد31-بازار .apk">دانلود نسخه 6.0.0 بازار</a> 
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    
+                     <a href="https://saycoder.com/downloads/sms-manager-myket-v6-0-0.apk">دانلود نسخه 6.0.0 مایکت</a> 
+                    <br>
+                    
+          </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>نسخه 6.0.6</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i> 1401/12/12 </h6>
+          <p>نسخه 6.0.6 دارای امکانات بیشتری نسبت به نسخه قبلی است با این حال ممکن است در بعضی گوشی ها درست کار نکند. </p>
+                    <br>
+     <br>
+          <a href="https://saycoder.com/downloads/پیام-ویژه-ورژن کد36-بازار .apk">دانلود نسخه6.0.6 بازار</a> 
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    
+                     <a href="https://saycoder.com/downloads/sms-manager-myket-v6-0-6.apk">دانلود نسخه 6.0.6 مایکت</a>
+                    <br>
+
+          <br>
+        </div>
+      </div>
+
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>این یک صفحه موقت برای اطلاع رسانی درمورد پیام ویژه است</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="https://saycoder.com" target="self">saycoder</a></p>
+</footer>
+
+</body>
+</html>
